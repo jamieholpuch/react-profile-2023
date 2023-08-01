424b747a4c2e83ad4cd05e7278e515352313552c
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-07-31 21:59:20 -0400

    removed blank target

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -38,7 +38,6 @@ export default function Navigation({ currentPage, handlePageChange}) {
       <li className="nav-item">
         <a
           href={resume}
-          target="_blank"
           onClick={() => handlePageChange('Resume')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
